73d1127203c25b2051da43ba4153e558906eeb8e
TomofumiSakai <garbee0107@gmail.com>
2020-07-25 22:05:41 +0900

    write parmeters

--- test_tethered_cell.py
+++ test_tethered_cell.py
@@ -7,4 +7,7 @@ sys.path.append(code_dir)
 from tethered_cell import tethered_cell
 
 path = os.path.join(code_dir, 'test_movies/1103_pH6.5_2016.04.15_17.17.19S.ihvideo-2.tif')
-tethered_cell(path)
+frame_number = 100
+FrameRate = 100.0
+CCW = 1
+tethered_cell(path, frame_number, FrameRate, CCW)
